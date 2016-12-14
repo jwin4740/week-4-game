5571228fee240d1bb106a4deb242423d7d391ca6
James <jwin4740@gmail.com>
2016-12-14 16:32:32 -0500

    asdf

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -105,9 +105,9 @@ function setRandGems () {
 
 $(function () {
 	    setInterval(function () {
-		    $('.star-1').fadeOut(150).delay(2000).fadeIn(300).fadeOut(150).delay(1254);
-	        $('.star-2').fadeOut(300).fadeIn(120).fadeOut(120).delay(1920);
-	        $('.star-3').fadeOut(150).delay(1200).fadeIn(300).fadeOut(150).delay(800);
-	        $('.star-4').fadeOut(700).fadeIn(300).fadeOut(160).delay(1350);
+		    $('#sparkle1').fadeOut(150).delay(2000).fadeIn(300).fadeOut(150).delay(1254);
+	        $('#sparkle2').fadeOut(300).fadeIn(120).fadeOut(120).delay(1920);
+	        $('#sparkle3').fadeOut(150).delay(1200).fadeIn(300).fadeOut(150).delay(800);
+	        $('#sparkle4').fadeOut(700).fadeIn(300).fadeOut(160).delay(1350);
 	    }, 1);
     });
