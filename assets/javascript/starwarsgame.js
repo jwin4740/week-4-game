81a32bfa9eb1dc710022cacba96db47f73f0b96b
James <jwin4740@gmail.com>
2016-12-16 12:45:30 -0500

    objects

--- assets/javascript/starwarsgame.js
+++ assets/javascript/starwarsgame.js
@@ -1,105 +1,147 @@
-
-$(document).ready()
-
-
-
-$(function () {
-	    setInterval(function () {
-		    $('#sparkle1').fadeOut(150).delay(2000).fadeIn(300).fadeOut(150).delay(1254);
-	        $('#sparkle2').fadeOut(300).fadeIn(120).fadeOut(120).delay(1920);
-	        $('#sparkle3').fadeOut(150).delay(1200).fadeIn(300).fadeOut(150).delay(800);
-	        $('#sparkle4').fadeOut(700).fadeIn(300).fadeOut(160).delay(1350);
-	    }, 1);
-
-
-
 $(document).ready(function() {
+var intro = document.getElementById("theme");
 
-      // Gets Link for Theme Song
-      var audioElement = document.createElement("audio");
-      audioElement.setAttribute("src", "Assets/captainplanet24.mp3");
+function fIntro() {
+    intro.play();
+}
+// fIntro();
+document.onkeyup = function(event) {
+    var userKey = event.key;
+    if (userKey === "d") {
+        fDraw();
+        setTimeout(fBuzz, 1000);
+        setTimeout(sBuzz, 2000);
+    }
 
-      // Theme Button
-      $(".themeButton").on("click", function() {
-        audioElement.play();
-      });
+    if (userKey === "s") {
+        fContact();
+    }
 
-      $(".pauseButton").on("click", function() {
-        audioElement.pause();
-      });
 
-      // Size Buttons
-      $(".normalButton").on("click", function() {
-        $(".captainplanet").animate({ height: "300px" });
-      });
+    if (userKey === "w") {
+        fSheath();
+    }
 
-      $(".growButton").on("click", function() {
-        $(".captainplanet").animate({ height: "500px" });
-      });
+    if (userKey === "w") {
+    	setTimeout(fSheath, 500);
+        resetSound();
+    }
 
-      $(".shrinkButton").on("click", function() {
-        $(".captainplanet").animate({ height: "100px" });
-      });
+};
 
-      // Visibility Buttons
-      $(".visButton").on("click", function() {
-        $(".captainplanet").animate({ opacity: "1" });
-      });
 
-      $(".invisButton").on("click", function() {
-        $(".captainplanet").animate({ opacity: "0.05" });
-      });
 
-      // Move Buttons
-      $(".upButton").on("click", function() {
-        $(".captainplanet").animate({ top: "-=200px" }, "normal");
-      });
 
-      $(".downButton").on("click", function() {
-        $(".captainplanet").animate({ top: "+=200px" }, "normal");
-      });
+var audio = document.getElementById("theme");
 
-      $(".leftButton").on("click", function() {
-        $(".captainplanet").animate({ left: "-=200px" }, "normal");
-      });
+function lessVolume() {
+    audio.volume = 0.5;
+}
+lessVolume();
 
-      $(".rightButton").on("click", function() {
-        $(".captainplanet").animate({ left: "+=200px" }, "normal");
-      });
+var draw = document.getElementById("saberdraw");
 
-      $(".backButton").on("click", function() {
-        $(".captainplanet").animate({ top: "50px", left: "80px" }, "fast");
-      });
+function fDraw() {
+    draw.play();
+}
 
-      // Move Buttons (Keyboard Down)
-      $(document).keyup(function(e) {
-        switch (e.which) {
-          case 40:
-            $("#kenobidiv").animate({ top: "+=200px" }, "normal");
-        }
-      });
+var buzz = document.getElementById("saberbuzz");
 
-      // Move Buttons (Keyboard Right)
-      $(document).keyup(function(e) {
-        switch (e.which) {
-          case 39:
-            $(".captainplanet").animate({ left: "+=200px" }, "normal");
-        }
-      });
+function fBuzz() {
+    buzz.play();
+}
 
-      // Move Buttons (Keyboard Up)
-      $(document).keyup(function(e) {
-        switch (e.which) {
-          case 38:
-            $(".captainplanet").animate({ top: "-=200px" }, "normal");
-        }
-      });
+var sbuzz = document.getElementById("sabersbuzz");
 
-      // Move Buttons (Keyboard Left)
-      $(document).keyup(function(e) {
-        switch (e.which) {
-          case 37:
-            $(".captainplanet").animate({ left: "-=200px" }, "normal");
-        }
-      });
-    });
+function sBuzz() {
+    sbuzz.play();
+}
+
+var contact = document.getElementById("sabercontact");
+
+function fContact() {
+    contact.play();
+}
+
+var sheath = document.getElementById("sabersheath");
+
+function fSheath() {
+    sheath.play();
+}
+
+function resetSound() {
+    buzz.pause();
+    sbuzz.pause();
+    buzz.currentTime = 0;
+    sbuzz.currentTime = 0;
+
+}
+
+var obiwanAttack = 5;
+
+var obiWan = {
+
+	healthPoints: 150,
+	attackPower: 5, 
+	counterAttackPower: 100,
+	
+
+	attack: function() {
+
+		this.attackPower = this.attackPower + obiwanAttack;
+		console.log(this.attackPower);
+	}, 
+
+	getHit: function() {
+
+		this.healthPoints = this.healthPoints + obiwanAttack;
+		console.log(this.healthPoints);
+	}, 
+
+	counterPunch: function() {
+
+		this.counterAttackPower = this.counterAttackPower + obiwanAttack;
+		console.log(this.counterAttackPower);
+	}, 
+
+};
+
+$(document).on("keypress", function(){
+
+
+obiWan.attack();
+obiWan.getHit();
+obiWan.counterPunch();
+
+
+});
+// var darthMaul = {
+
+// 	HealthPoints: 250
+// 	AttackPower: 5,
+// 	CounterAttackPower: 100,
+// 	Defender: true;
+
+// };
+
+// var quiGon = {
+
+// 	HealthPoints: 300,
+// 	AttackPower: 5,
+// 	CounterAttackPower: 100,
+// 	Defender: true;
+
+// };
+
+// var countDooku = {
+
+// 	HealthPoints: 400,
+// 	AttackPower: 5,
+// 	CounterAttackPower: 100,
+// 	Defender: true;
+
+// };
+
+
+});
+
